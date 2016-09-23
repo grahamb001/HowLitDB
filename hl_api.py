cc3e8b4c533aa8d7d477d4124be9bab19ef961b4
Phillip Smith <psmith586@yahoo.com>
2016-09-23 14:46:08 -0700

    Update hl_api.py
    changed a bit of the ordering to make it more concise. still got a few bugs

--- hl_api.py
+++ hl_api.py
@@ -6,9 +6,6 @@ import requests
 url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=dubstep&api_key=bf14ea0f02f9152e1b0e6dde7def5e2d&format=json&limit=100"
 r = requests.get(url)
 
-#list to scrape requests
-genre_tag = ['rap', 'dubstep', 'electronica', 'trance', 'house']
-
 def main():
 	statusCheck()
 	getData()
@@ -23,11 +20,16 @@ def statusCheck():
 	
 #scrape full list
 def getData():
+	#shorten url by adding api key variable
+	api_key = "&api_key=bf14ea0f02f9152e1b0e6dde7def5e2d&format=json&limit=100"
+	#list to scrape api by tag
+	genre_tag = ['rap', 'dubstep', 'electronica', 'trance', 'house']
+	
 	for x in genre_tag:
-		r = requests.get("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=" + x + "&api_key=bf14ea0f02f9152e1b0e6dde7def5e2d&format=json&limit=100")
+		r = requests.get("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=" + x + api_key
 		resp = r.json()
-		for i in resp['artist']:
-			print(i['name'])
+		print(resp['artist'])
+		
 		
 			
 main()
