5c26b6860848692d1700d6d06c9c4679401a2522
Phillip Smith <psmith586@yahoo.com>
2016-10-06 01:28:24 -0700

    Update hl_api.py
    updated the code to get the data into a text file successfully. will work on parsing from here.

--- hl_api.py
+++ hl_api.py
@@ -24,15 +24,19 @@ def getData():
 	api_key = "&api_key=bf14ea0f02f9152e1b0e6dde7def5e2d&format=json&limit=100"
 	#list to scrape api by tag
 	genre_tag = ['rap', 'dubstep', 'electronica', 'trance', 'house']
+	
+	data_file = open('data.txt', 'w')
+	
 	#loop thru the list concatenated with the request url
 	for i in genre_tag:
-		r = requests.get("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=", + i, + api_key
+		r = requests.get("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=" + i + api_key)
 		resp = r.json()
-		print(resp["topartists"])
+		json.dump(r.json(), data_file, sort_keys = True, indent = 4, ensure_ascii = True)
 		
 		
 			
 main()
+
 
 
 	
