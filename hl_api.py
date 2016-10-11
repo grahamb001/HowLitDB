3198fd99af9819516431e55782f5b8165db3e00b
Phillip Smith <psmith586@yahoo.com>
2016-10-11 16:18:59 -0700

    Update hl_api.py
    untested update. do not have environment to test yet

--- hl_api.py
+++ hl_api.py
@@ -1,12 +1,15 @@
 import json
 import requests
+import MySQLdb
 
 url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=dubstep&api_key=bf14ea0f02f9152e1b0e6dde7def5e2d&format=json&limit=100"
 r = requests.get(url)
+data = []
 
 def main():
 	statusCheck()
 	getData()
+	updateArtists(data)
 
 def statusCheck():
 	r.status_code
@@ -27,7 +30,21 @@ def getData():
 		resp = r.json()
 		artists = resp['topartists']['artist']
 		for a in range(0,len(artists)):
-			 	print(artists[a]['name'])
-	print(resp['topartists']['artist'][0]['name'])
+			data.append(artists[a]['name'])
+			
+def updateArtists(list):
+    	#connect to db
+	db = MySQLdb.connect("localhost","root","barry1","how_lit_db" )
+	cursor = db.cursor()
+	#append db with loop
+    	for x in list:
+		cursor.execute("UPDATE artist_tbl SET artist_name =" + x)
+		db.commit()
 
 main()
+
+
+	
+	
+	
+	
